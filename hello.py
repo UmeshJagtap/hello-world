b465d57c75f40fc3ff809405e39296a59210fb42
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2023-05-21 13:31:37 +0530

    Current time
    Added Display Current Time functionality.

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
+# Display Grettings
+
 print("Hello, World")
 print("__ Good Morning __")
+
+# Display Time
+from pytz import timezone
+from datetime import datetime
+
+ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%m-%d %H:%M:%S.%f')
+print(ind_time)
